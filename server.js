a2bae1d1a249ead10669cdc0a660c9649e874d89
Shiraz <sherrazahmed.97@gmail.com>
2019-03-13 15:14:19 +0500

    initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.send('Hello Express!');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        title: 'About page',
+        currentYear: new Date().getFullYear(),
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
